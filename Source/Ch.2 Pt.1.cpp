64e6c09af576580bb86930edc9a4d67f1ae2465f
Casthely Simeus <Cas_Simeus@Yahoo.com>
2019-08-16 11:33:41 -0400

    Workout 1

--- Source/Ch.2 Pt.1.cpp
+++ Source/Ch.2 Pt.1.cpp
@@ -31,87 +31,116 @@ arm.rotateCounterClockwise(); //demonstrates CamelCase
 
 /*
  1)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:		head
+ action 1:	looks to the left
+ action 2:	looks to the right
+ action 3:	sways back and forth
  */
+head.looksLeft();
+head.looksRight();
+head.swaysBackAndForth();
 
 
 /*
 2)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		hand
+action 1:	puts on clothes
+action 2:	takes off clothes
+action 3:	washes clothes
 */
+hand.putsOnClothes();
+hand.takesOffClothes();
+hand.washesClothes();
 
 /*
 3)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		keyboard
+action 1:	lights up
+action 2:	has rounded edges
+action 3:	is compact sized
 */
+keyboard.lightsUp();
+keyboard.roundedEdges();
+keyboard.compactSize();
+
 
 
 /*
 4)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		daw
+action 1:	used to record music
+action 2:	used for music production
+action 3:	used to edit audio
 */
+daw.recordMusic();
+daw.musicProduction();
+daw.editMusic();
 
 /*
 5)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		interface
+action 1:	records incoming sounds
+action 2:	controls monitor volume level
+action 3:	converts audio to digital signal
 */
+interface.recordsSounds();
+interface.controlsMonitorVolumeLevel();
+interface.convertsAudioToDigitalSignal();
 
 
 /*
 6)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		clippers
+action 1:	used to cut hair
+action 2:	a barbers tool
+action 3:	gets hot after prolonged use
 */
+clippers.cutHair();
+clippers.barbersTool();
+clippers.getsHotAfterProlongedUse();
 
 /*
 7)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		pen
+action 1:	has ink inside of it
+action 2:	used to write
+action 3:	can be used as ah weapon
 */
+pen.hasInk();
+pen.writes();
+pen.canBeAhWeapon();
 
 
 /*
 8)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		1176
+action 1:	turn knobs for input and output 
+action 2:	push buttons for ratios
+action 3:	compresses audio signals
 */
+1176.turnKnobForInputOutput();
+1176.pushButtonsRatio();
+1176.compressAudioSignals();
 
 /*
 9)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		baby		
+action 1:	cries when hungry
+action 2:	puts fingers in mouth
+action 3:	holds onto fingers
 */
+baby.criesWhenHungry();
+baby.fingersInMouth();
+baby.holdsFingers();
 
 
 /*
 10)
-Noun:
-action 1:
-action 2:
-action 3:
+Noun:		skin
+action 1:	sweats when hot
+action 2:	heals on it's own when cut
+action 3:	changes complexion in sun
 */
-
-
+skin.sweats();
+skin.healsWhenCut();
+skin.changesComplexion();
