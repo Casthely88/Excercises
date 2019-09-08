fefe87ce8d0b5e4b1b83905cb0a88eef42ba2c04
Casthely Simeus <Cas_Simeus@Yahoo.com>
2019-09-08 18:25:24 -0400

    Finish on Macbook Pro

--- Source/Ch.2 Pt.10.h
+++ Source/Ch.2 Pt.10.h
@@ -1,130 +1,373 @@
 /*
  Chapter 2 - Part 10
  Scope and Lifetime tasks
- 
+
  video covered:
  variable scope and lifetime relative to { }
  while loops
  for loops()
- 
+
  tasks
  1) open the coliru link you submitted where you added and implemented constructors to your 10 user-defined types.
- 
+
  2) click the Edit button on it.
- 
+
  3) add some new member functions to your types.
- 
+
  4) use while() or for() loops to do something interesting inside these new member functions.
  a) for example: have a loop that modifies a member variable of some object created outside the loop.
  b) when that member variable reaches a certain threshold, return it mid-loop.
  c) maybe use function parameters to control the starting value of that member variable or control the threshold
- 
+
  5) call those new member functions in main()
- 
+
  6) use std::cout statements to print out information about what your loops did.
- 
+
  7) click the [Compile, Link and Run] button to make sure you don't have any errors.
- 
+
  share the url into the #ch2_p10 channel once you're done.
- 
+
  example:
  */
 
 #include <iostream>
 struct Bar
 {
-    int num = 0;
-    Bar(int n) : num(n) { }
+	int num = 0;
+	Bar(int n) : num(n) { }
 };
 struct Foo
 {
-    Bar scopeLifetimeFunc(int threshold, int startingVal) //3), 4c)
-    {
-        Bar bar(startingVal);                //4a)
-        while (bar.num < threshold)         //4a)
-        {
-            bar.num += 1;                    //4a)
-            
-            if (bar.num >= threshold)       //4b)
-                return bar;
-        }
-        
-        return Bar{ -1 }; //if your startingValue >= threshold, the while loop never runs
-    }
+	Bar scopeLifetimeFunc(int threshold, int startingVal) //3), 4c)
+	{
+		Bar bar(startingVal);                //4a)
+		while (bar.num < threshold)         //4a)
+		{
+			bar.num += 1;                    //4a)
+
+			if (bar.num >= threshold)       //4b)
+				return bar;
+		}
+
+		return Bar{ -1 }; //if your startingValue >= threshold, the while loop never runs
+	}
+};
+
+/*
+ Example B)
+ */
+
+struct SmartPhone
+{
+	int screens = 1;
+	SmartPhone(int s) : screens(s) { }
+};
+struct Signal
+{
+	SmartPhone strengthOfSignal(int threshold, int startingVal)
+	{
+		SmartPhone smartPhone(startingVal);
+		while (smartPhone.screens < threshold)
+		{
+			smartPhone.screens += 1;
+
+			if (smartPhone.screens >= threshold)
+				return smartPhone;
+		}
+
+		return SmartPhone{ -1 };
+	}
+};
+
+/*
+ Example C)
+ */
+
+struct Workout
+{
+	int timeWorkingOut = 1;
+	Workout(int t) : timeWorkingOut(t) { }
+};
+struct Trainer
+{
+	Workout cardio(int swimming, int walking)
+	{
+		Workout workout(walking);
+		while (workout.timeWorkingOut < swimming)
+		{
+			workout.timeWorkingOut += 1;
+
+			if (workout.timeWorkingOut >= swimming)
+				return workout;
+		}
+
+		return Workout{ 0 };
+	}
 };
 
 /*
  1)
  */
 
-struct SmartPhone
+struct Airline
 {
-    int screens = 1;
-    SmartPhone(int s) : screens(s) { }
+	int fleetSize = 12;
+	Airline(int f) : fleetSize(f) { }
 };
-struct Signal
+struct Plane
 {
-    SmartPhone strengthOfSignal(int threshold, int startingVal)
-    {
-        SmartPhone smartPhone(startingVal);
-        while (smartPhone.screens < threshold)
-        {
-            smartPhone.screens += 1;
-            
-            if (smartPhone.screens >= threshold)
-                return smartPhone;
-        }
-        
-        return SmartPhone{ -1 };
-    }
+	Airline sections(int leftside, int rightside)
+	{
+		Airline airline(rightside);
+		while (airline.fleetSize < leftside)
+		{
+			airline.fleetSize += 1;
+
+			if (airline.fleetSize >= leftside)
+				return airline;
+		}
+
+		return Airline{ 0 };
+	}
 };
 
 /*
  2)
  */
 
-struct Workout
+struct Team
 {
-    int timeWorkingOut = 1;
-    Workout(int t) : timeWorkingOut(t) { }
+	int players = 2;
+	Team(int p) : players(p) { }
 };
-struct Trainer
+struct Athlete
 {
-    Workout cardio(int swimming, int walking)
-    {
-        Workout workout(walking);
-        while (workout.timeWorkingOut < swimming)
-        {
-            workout.timeWorkingOut += 2;
-            
-            if (workout.timeWorkingOut >= swimming)
-                return workout;
-        }
-        
-        return Workout{ 2 };
-    }
+	Team position(int pointguard, int shootingguard)
+	{
+		Team team(shootingguard);
+		while (team.players < pointguard)
+		{
+			team.players += 4;
+
+			if (team.players >= pointguard)
+				return team;
+		}
+
+		return Team{ 0 };
+	}
 };
 
 /*
  3)
  */
 
+struct Employee
+{
+	int manager = 1;
+	Employee(int m) : manager(m) { }
+};
+struct Pay
+{
+	Employee hourlyRate(int supervisor, int leadWorker)
+	{
+		Employee employee(supervisor);
+		while (employee.manager < leadWorker)
+		{
+			employee.manager += 21;
 
+			if (employee.manager >= leadWorker)
+				return employee;
+		}
+
+		return Employee{ 0 };
+	}
+};
+
+/*
+ 4)
+ */
+struct College
+{
+	int courses = 3;
+	College(int c) : courses(c) { }
+};
+struct Student
+{
+	College timeStudying(int math, int reading)
+	{
+		College college(reading);
+		while (college.courses < math)
+		{
+			college.courses += 1;
+
+			if (college.courses >= math)
+				return college;
+		}
+
+		return College{ -1 };
+	}
+};
+
+/*
+ 5)
+ */
+
+struct Television
+{
+	bool smartTV = true;
+	int hdmiPorts = 4;
+	bool visio = true;
+	int channels;
+	int cableCompany;
+	bool hasRemote;
+
+	Television();
+
+	void chris()
+	{
+		std::cout << "Time to watch my shows" << std::endl;
+		std::cout << "New batteries for the remote" << std::endl;
+	}
+};
+
+Television::Television() : smartTV(true), hdmiPorts(4), visio(true), channels(72), cableCompany(0), hasRemote(true) {}
+
+/*
+ 6)
+ */
+
+struct Compressor
+{
+	int rangeOfCompression = 20;
+	bool vuMeter = true;
+	int threshold = -20;
+	int makeUpGain = 20;
+	int attackRange;
+	int releaseRange;
+	int inputs;
+
+	Compressor()
+	{
+		attackRange = 7;
+		releaseRange = 7;
+		inputs = 3;
+	}
+
+	void elevenSeventySix()
+	{
+		std::cout << "This KT-76 is great!!!" << std::endl;
+	}
+
+};
+
+/*
+ 7)
+ */
+
+struct Typewriter
+{
+	int age = 32;
+	int lettersWritten = 0;
+	bool ink;
+	int scrollsOfPaper;
+
+	Typewriter()
+	{
+		ink = true;
+		scrollsOfPaper = 3;
+	}
+
+	void diana()
+	{
+		std::cout << "I've got only 3 pages left" << std::endl;
+	}
+};
+
+/*
+ 8)
+ */
+
+struct VideoGame
+{
+	int players = 1;
+	bool rpg = true;
+	char name = 'L';
+};
+struct Character
+{
+	bool greenOutfit = true;
+	bool rescuePrincess = true;
+	int fightingSkill;
+	int b;
+
+	Character()
+	{
+		fightingSkill = 86;
+		b = 23;
+	}
+	void link()
+	{
+		std::cout << "Gotta get to Zelda before it's too late" << std::endl;
+	}
+
+};
+
+/*
+ 9)
+ */
+
+struct Rack
+{
+	int equipment = 8;
+	int rackSpace = 15;
+	bool emptySpaces = true;
+
+	Rack();
+};
+
+Rack::Rack() {}
+
+/*
+ 10)
+ */
+
+struct Male
+{
+	int fatigueLevel = 8;
+	bool readyToGiveUp = false;
+	char motivationLevel = 'A';
+
+	Male();
+};
+
+Male::Male() {}
 
 int main()
 {
-    Foo foo;
-    Signal signal;
-    Trainer trainer;
-    
-    auto bar = foo.scopeLifetimeFunc(3, 1);        //5)
-    auto smartPhone = signal.strengthOfSignal(5, 0);
-    auto workout = trainer.cardio(1, 2);
-    
-    std::cout << "bar.num: " << bar.num << std::endl;     //6)
-    std::cout << "The signal of the smartphone: " << smartPhone.screens << std::endl;
-    std::cout << "Hours a day swimming? " << workout.timeWorkingOut << std::endl;
+	Foo foo;
+	Signal signal;
+	Trainer trainer;
+	Plane plane;
+	Athlete athlete;
+	Pay pay;
+	Student student;
+	Television television;
+	Compressor compressor;
+	Typewriter typewriter;
+
+	auto bar = foo.scopeLifetimeFunc(3, 1);        //5)
+	auto smartPhone = signal.strengthOfSignal(5, 0);
+	auto workout = trainer.cardio(1, -1);
+	auto airline = plane.sections(3, 0);
+	auto team = athlete.position(4, 0);
+	auto employee = pay.hourlyRate(2, 21);
+	auto college = student.timeStudying(4, 0);
+
+	std::cout << "bar.num: " << bar.num << std::endl;     //6)
+	std::cout << "The signal of the smartphone: " << smartPhone.screens << std::endl;
+	std::cout << "Hours a day swimming? " << workout.timeWorkingOut << std::endl;
+	std::cout << "Each row on the rightside of the plane has how many seats? " << airline.fleetSize << std::endl;
+	std::cout << "How many pointguards are on the team? " << team.players << std::endl;
+	std::cout << "How much does the supervisor make per hour? " << employee.manager << std::endl;
+	std::cout << "How many days should I study for math? " << college.courses << std::endl;
 }
-
-
-
--- Source/Ch.2 Pt.9.h
+++ Source/Ch.2 Pt.9.h
@@ -17,6 +17,10 @@
  */
 
 #include <iostream>
+ /*
+ Example)
+ */
+
 struct Foo
 {
 	int a; //a member variable
@@ -28,9 +32,285 @@ struct Foo
 	}
 };
 
+/*
+1)
+*/
+
+struct Plane
+{
+	double weight = 386.265;
+	int seats = 286;
+	int litersOfFuel = 184;
+	int crewMembers;
+	float milesFlying;
+
+	Plane();
+
+	void pilot()
+	{
+		std::cout << "This is your captain Marcus speaking" << std::endl;
+		std::cout << "We have eight members on board to assist you with whatever your needs may be" << std::endl;
+	}
+
+
+};
+
+Plane::Plane() : weight(386.265), seats(286), litersOfFuel(184), crewMembers(8), milesFlying(663.9f) {}
+
+/*
+ 2)
+ */
+
+struct Athlete
+{
+	int age;
+	int points = 15386;
+	bool football = false;
+	bool jumpShots = true;
+	bool layups;
+	float pointsPerGame;
+
+	Athlete();
+
+	void shootingGuard()
+	{
+		std::cout << "I gotta work on my game" << std::endl;
+		std::cout << "I need to make 4 layups in todays game" << std::endl;
+	}
+
+};
+
+Athlete::Athlete() : age(30), points(15386), football(false), jumpShots(true), layups(true), pointsPerGame(24.6f) {}
+
+/*
+ 3)
+ */
+
+struct Pay
+{
+	void halfBusinessDay();
+	int timeAndAhHalf();
+};
+
+void Pay::halfBusinessDay() {}
+int Pay::timeAndAhHalf() { return 2; }
+
+struct Employee
+{
+	bool manager = false;
+	bool supervisor = true;
+	int daysPerWeek = 5;
+	float paycheck = 535.73f;
+	int holidayPay;
+	char workerRating;
+	int lengthOfEmployment;
+
+	Employee();
+
+	void carpenter()
+	{
+		std::cout << "My paycheck is going to be fat!!!" << std::endl;
+		std::cout << "I've been here for fifthteen months" << std::endl;
+	}
+
+	void overtime(float howMuchTime, bool holidays);
+
+	Pay thanksgiving;
+	Pay christmas;
+
+};
+
+Employee::Employee() : manager(false), supervisor(true), daysPerWeek(5), paycheck(535.73f), workerRating('B'), lengthOfEmployment(15) {}
+
+void Employee::overtime(float howMuchTime, bool holidays)
+{
+	if (holidays == true)
+	{
+		thanksgiving.timeAndAhHalf();
+		christmas.timeAndAhHalf();
+	}
+	else
+	{
+		christmas.halfBusinessDay();
+		thanksgiving.halfBusinessDay();
+	}
+
+	holidayPay += thanksgiving.timeAndAhHalf() + christmas.timeAndAhHalf();
+}
+
+/*
+ 4)
+ */
+
+struct Student
+{
+	bool female = true;
+	char grade = 'B';
+	int classmates;
+	int hoursOfStudying;
+	float lunchbreak;
+
+	Student();
+
+	void lunchroom()
+	{
+		std::cout << "I hate this table" << std::endl;
+		std::cout << "Good thing I've already studied!!!" << std::endl;
+	}
+};
+
+Student::Student() : female(true), grade('B'), classmates(15), hoursOfStudying(3), lunchbreak(0.42f) {}
+
+/*
+ 5)
+ */
+
+struct Television
+{
+	bool smartTV = true;
+	int hdmiPorts = 4;
+	bool visio = true;
+	int channels;
+	int cableCompany;
+	bool hasRemote;
+
+	Television();
+
+	void chris()
+	{
+		std::cout << "Time to watch my shows" << std::endl;
+		std::cout << "New batteries for the remote" << std::endl;
+	}
+};
+
+Television::Television() : smartTV(true), hdmiPorts(4), visio(true), channels(72), cableCompany(0), hasRemote(true) {}
+
+/*
+ 6)
+ */
+
+struct Compressor
+{
+	int rangeOfCompression = 20;
+	bool vuMeter = true;
+	int threshold = -20;
+	int makeUpGain = 20;
+	int attackRange;
+	int releaseRange;
+	int inputs;
+
+	Compressor()
+	{
+		attackRange = 7;
+		releaseRange = 7;
+		inputs = 3;
+	}
+
+	void elevenSeventySix()
+	{
+		std::cout << "This KT-76 is great!!!" << std::endl;
+	}
+
+};
+
+/*
+ 7)
+ */
+
+struct Typewriter
+{
+	int age = 32;
+	int lettersWritten = 0;
+	bool ink;
+	int scrollsOfPaper;
+
+	Typewriter()
+	{
+		ink = true;
+		scrollsOfPaper = 3;
+	}
+
+	void diana()
+	{
+		std::cout << "I've got only 3 pages left" << std::endl;
+	}
+};
+
+/*
+ 8)
+ */
+
+struct VideoGame
+{
+	int players = 1;
+	bool rpg = true;
+	char name = 'L';
+
+	struct Character
+	{
+		bool greenOutfit = true;
+		bool rescuePrincess = true;
+		int fightingSkill;
+		int b;
+
+		Character()
+		{
+			fightingSkill = 86;
+			b = 23;
+		}
+		void link()
+		{
+			std::cout << "Gotta get to Zelda before it's too late" << std::endl;
+		}
+	};
+};
+
+/*
+ 9)
+ */
+
+struct Rack
+{
+	int equipment = 8;
+	int rackSpace = 15;
+	bool emptySpaces = true;
+
+	Rack();
+};
+
+Rack::Rack() {}
+
+/*
+ 10)
+ */
+
+struct Male
+{
+	int fatigueLevel = 8;
+	bool readyToGiveUp = false;
+	char motivationLevel = 'A';
+
+	Male();
+};
+
+Male::Male() {}
+
+/*
+End)
+*/
 int main()
 {
-	Foo foo; //instantiating a Foo in main()
-	foo.printThing(); //calling a member function of the instance that was instantiated.
+	Plane plane;
+	Athlete athlete;
+	Employee employee;
+	Television television;
+	Compressor compressor;
+	Typewriter typewriter;
+
+	plane.pilot();
+	athlete.shootingGuard();
+	employee.carpenter();
+	television.chris();
+	compressor.elevenSeventySix();
+	typewriter.diana();
 }
-#pragma once
